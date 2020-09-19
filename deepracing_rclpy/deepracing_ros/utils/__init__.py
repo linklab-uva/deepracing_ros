9aaabb5b82d77fa85745366abdc33727f3053b62
Trent Weiss <ttw2xk@virginia.edu>
2020-09-19 08:39:08 -0400

    more explicit typing

--- deepracing_rclpy/deepracing_ros/utils/__init__.py
+++ deepracing_rclpy/deepracing_ros/utils/__init__.py
@@ -1,12 +1,12 @@
 import rclpy
 import threading
 class AsyncSpinner():
-    def __init__(self, executor):
-        self.executor = executor
-        self.thread = threading.Thread(target=self.executor.spin, daemon=True)
+    def __init__(self, executor : rclpy.executors.Executor):
+        self.executor : rclpy.executors.Executor = executor
+        self.thread : threading.Thread = threading.Thread(target=self.executor.spin, daemon=True)
     def spin(self):
         self.thread.start()
     def addNode(self, node):
         self.executor.add_node(node)
-    def shutdown(self):
-        self.executor.shutdown()
+    def shutdown(self, timeout_sec=0.0):
+        self.executor.shutdown(timeout_sec = timeout_sec)
