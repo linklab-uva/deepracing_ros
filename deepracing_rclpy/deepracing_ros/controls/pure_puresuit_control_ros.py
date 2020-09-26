8e06480af29c7569572e858cab4641121f427ef5
Trent Weiss <ttw2xk@virginia.edu>
2020-09-26 05:33:30 -0400

    better print

--- deepracing_rclpy/deepracing_ros/controls/pure_puresuit_control_ros.py
+++ deepracing_rclpy/deepracing_ros/controls/pure_puresuit_control_ros.py
@@ -174,7 +174,7 @@ class PurePursuitControllerROS(Node):
         pass
 
     def poseCallback(self, pose_msg : PoseStamped):
-        print("Got a new pose: " + str(pose_msg))
+        self.get_logger().info("Got a new pose: " + str(pose_msg))
         
         R = torch.from_numpy(Rot.from_quat( np.array([pose_msg.pose.orientation.x, pose_msg.pose.orientation.y, pose_msg.pose.orientation.z, pose_msg.pose.orientation.w], dtype=np.float64) ).as_matrix()).double()
         v = torch.from_numpy(np.array([pose_msg.pose.position.x, pose_msg.pose.position.y, pose_msg.pose.position.z], dtype=np.float64 ) )
@@ -192,7 +192,7 @@ class PurePursuitControllerROS(Node):
             self.get_logger().error("Unable to acquire semaphore to setting the pose data")
 
     def velocityCallback(self, velocity_msg : TwistStamped):
-        print("Got a new velocity: " + str(velocity_msg))
+        self.get_logger().info("Got a new velocity: " + str(velocity_msg))
         linearvel = velocity_msg.twist.linear
         vel = np.array( (linearvel.x, linearvel.y, linearvel.z), dtype=np.float64)
         speed = la.norm(vel)
