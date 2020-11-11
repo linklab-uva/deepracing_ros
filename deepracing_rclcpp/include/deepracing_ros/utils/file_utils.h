46eefac07e3a15e23962dc372c89aaa0791fa49e
Trent Weiss <ttw2xk@virginia.edu>
2020-11-11 09:12:58 -0500

    use "visilibity_control.hpp" model for dll exports

--- deepracing_rclcpp/include/deepracing_ros/utils/file_utils.h
+++ deepracing_rclcpp/include/deepracing_ros/utils/file_utils.h
@@ -1,16 +1,16 @@
 #ifndef DEEPRACING_ROS_FILE_UTILS_H
 #define DEEPRACING_ROS_FILE_UTILS_H
+#include <deepracing_ros/visibility_control.hpp>
 #include <vector>
 #include <string>
-#ifndef DEEPRACING_ROS_PUBLIC
-    #define DEEPRACING_ROS_PUBLIC
-#endif
+
+
 namespace deepracing_ros
 {
     class FileUtils
     {
         public:
-            static inline DEEPRACING_ROS_PUBLIC char pathsep()
+            static inline DEEPRACING_RCLCPP_PUBLIC char pathsep()
             {
                 #ifdef _MSC_VER
                     return ';';
@@ -18,8 +18,8 @@ namespace deepracing_ros
                     return ':';
                 #endif
             }
-            static DEEPRACING_ROS_PUBLIC std::string findFile(const std::string& file, const std::vector<std::string> & search_dirs);
-            static DEEPRACING_ROS_PUBLIC std::vector<std::string> split(const std::string& str, char delim=pathsep());
+            static DEEPRACING_RCLCPP_PUBLIC std::string findFile(const std::string& file, const std::vector<std::string> & search_dirs);
+            static DEEPRACING_RCLCPP_PUBLIC std::vector<std::string> split(const std::string& str, char delim=pathsep());
     };
 }
 
