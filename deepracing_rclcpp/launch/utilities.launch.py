b5fb1e9789da68070f16ca44f8fe8e8d5218dad0
Trent Weiss <ttw2xk@virginia.edu>
2020-12-17 18:11:33 -0500

    search for trackfiles in environment variable

--- deepracing_rclcpp/launch/utilities.launch.py
+++ deepracing_rclcpp/launch/utilities.launch.py
@@ -2,10 +2,10 @@
 
 from launch import LaunchDescription
 import launch_ros.actions
-
+import os
 
 def generate_launch_description():
     return LaunchDescription([
-        launch_ros.actions.Node(package='deepracing_rclcpp', executable='boundary_publisher', output='screen'),
+        launch_ros.actions.Node(package='deepracing_rclcpp', executable='boundary_publisher', output='screen', parameters=[{"track_search_dirs": os.getenv("F1_TRACK_DIRS","").split(os.pathsep)}]),
         launch_ros.actions.Node(package='deepracing_rclcpp', executable='tf_updater', output='screen')
     ])
