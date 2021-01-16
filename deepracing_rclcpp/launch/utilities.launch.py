53e8fb2831fda1848ee8a485a87a9aa7dea05dc6
Trent Weiss <ttw2xk@virginia.edu>
2021-01-16 12:30:21 -0500

    update launch file for tf updater

--- deepracing_rclcpp/launch/utilities.launch.py
+++ deepracing_rclcpp/launch/utilities.launch.py
@@ -1,11 +1,16 @@
 """Launch the cpp_code executable in this package"""
 
-from launch import LaunchDescription
-import launch_ros.actions
 import os
+import launch_ros.actions
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    config_dir = os.path.join(get_package_share_directory("deepracing_rclcpp"),"config")
+    config_file = os.path.join(config_dir, "tf_updater.yaml")
     return LaunchDescription([
         launch_ros.actions.Node(package='deepracing_rclcpp', executable='boundary_publisher', output='screen', parameters=[{"track_search_dirs": os.getenv("F1_TRACK_DIRS","").split(os.pathsep)}], remappings=[("/motion_data", "/cropped_publisher/motion_data"), ("/session_data", "/cropped_publisher/session_data")]),
-        launch_ros.actions.Node(package='deepracing_rclcpp', executable='tf_updater', output='screen', remappings=[("/motion_data", "/cropped_publisher/motion_data"), ("/session_data", "/cropped_publisher/session_data")])
+        launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_tf_updater', executable='tf_updater', output='screen', parameters=[config_file], remappings=[("/motion_data", "/cropped_publisher/motion_data"), ("/session_data", "/cropped_publisher/session_data")])
     ])
