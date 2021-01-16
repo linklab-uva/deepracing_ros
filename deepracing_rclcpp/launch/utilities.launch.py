06bb7da060a5882cbbcc9bbef23d2df3b1af66f6
Trent Weiss <ttw2xk@virginia.edu>
2021-01-16 15:58:04 -0500

    allow setting config file from command prompt

--- deepracing_rclcpp/launch/utilities.launch.py
+++ deepracing_rclcpp/launch/utilities.launch.py
@@ -9,8 +9,9 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     config_dir = os.path.join(get_package_share_directory("deepracing_rclcpp"),"config")
-    config_file = os.path.join(config_dir, "tf_updater.yaml")
-    return LaunchDescription([
-        launch_ros.actions.Node(package='deepracing_rclcpp', executable='boundary_publisher', output='screen', parameters=[{"track_search_dirs": os.getenv("F1_TRACK_DIRS","").split(os.pathsep)}], remappings=[("/motion_data", "/cropped_publisher/motion_data"), ("/session_data", "/cropped_publisher/session_data")]),
-        launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_tf_updater', executable='tf_updater', output='screen', parameters=[config_file], remappings=[("/motion_data", "/cropped_publisher/motion_data"), ("/session_data", "/cropped_publisher/session_data")])
-    ])
+    config_file = DeclareLaunchArgument("config_file", default_value=os.path.join(config_dir, "tf_updater.yaml"))
+    
+    entries = [config_file]
+    entries.append(launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_boundary_publisher', executable='boundary_publisher', output='screen', parameters=[{"track_search_dirs": os.getenv("F1_TRACK_DIRS","").split(os.pathsep)}], remappings=[("/motion_data", "/cropped_publisher/motion_data"), ("/session_data", "/cropped_publisher/session_data")]))
+    entries.append(launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_tf_updater', executable='tf_updater', output='screen', parameters=[LaunchConfiguration(config_file.name)], remappings=[("/motion_data", "/cropped_publisher/motion_data"), ("/session_data", "/cropped_publisher/session_data")]))
+    return LaunchDescription(entries)
