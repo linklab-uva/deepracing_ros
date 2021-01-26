3bcc972d06cd5544b9c5c28a84dd17171e7a58ce
Trent Weiss <ttw2xk@virginia.edu>
2021-01-25 22:18:28 -0500

    fix an oopsie

--- deepracing_rclpy/launch/vehicle_state_publisher.launch.py
+++ deepracing_rclpy/launch/vehicle_state_publisher.launch.py
@@ -12,11 +12,6 @@ from launch.conditions import IfCondition
 
 def generate_launch_description():
    namespace = DeclareLaunchArgument("ns", default_value="")
-   params_file = DeclareLaunchArgument("params_file")
-   vjoy = DeclareLaunchArgument("vjoy", default_value="True")
-   entries = [namespace, params_file, vjoy]
-   remappings = list({"car_pose" : "/ego_vehicle/pose", "car_velocity" : "/ego_vehicle/velocity"}.items())
-   parameters=[LaunchConfiguration(params_file.name)]
-   entries.append( launch_ros.actions.Node(name="vjoy_interface", package='deepracing_rclpy', executable='vjoy_control_node', output='both', condition = IfCondition(LaunchConfiguration(vjoy.name)) ) )
-   entries.append( launch_ros.actions.Node(name="pure_pursuit_control", package='deepracing_rclpy', executable='pure_pursuit_bezier', output='both', parameters=parameters, remappings=remappings, namespace=LaunchConfiguration(namespace.name) ) )
+   entries = [namespace]
+   entries.append( launch_ros.actions.Node(name="vehicle_state_publisher", package='deepracing_rclpy', executable='vehicle_state_publisher', output='both', namespace=LaunchConfiguration(namespace.name) ) )
    return launch.LaunchDescription(entries)
