a10dbda1f5a41b85e773b4091b5924942fca68bd
Trent Weiss <ttw2xk@virginia.edu>
2021-09-16 09:15:05 -0400

    bash script for adding source dirs to pythonpath

---
+++ addpythonpathsrc.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do cd `dirname "$SCRIPT_PATH"`; SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+echo $SCRIPT_PATH
+extrapythonpaths=$SCRIPT_PATH/deepracing_rclpy
+echo $extrapythonpaths
+
+if [[ -z "${PYTHONPATH}" ]]; then
+  export PYTHONPATH=${extrapythonpaths}
+else
+  export PYTHONPATH=${extrapythonpaths}:${PYTHONPATH}
+fi
