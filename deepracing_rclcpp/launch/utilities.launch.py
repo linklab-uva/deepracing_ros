efea736899e9052d72ca9581be8d004288022070
Trent Weiss <ttw2xk@virginia.edu>
2021-10-29 18:07:34 -0400

    add sim time option

--- deepracing_rclcpp/launch/utilities.launch.py
+++ deepracing_rclcpp/launch/utilities.launch.py
@@ -10,8 +10,9 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     config_dir = os.path.join(get_package_share_directory("deepracing_rclcpp"),"config")
     config_file = DeclareLaunchArgument("config_file", default_value=os.path.join(config_dir, "tf_updater.yaml"))
+    use_sim_time = DeclareLaunchArgument("use_sim_time", default_value="false")
     
-    entries = [config_file]
-    entries.append(launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_boundary_publisher', executable='boundary_publisher', output='screen', parameters=[{"track_search_dirs": os.getenv("F1_TRACK_DIRS","").split(os.pathsep)}], remappings=[("motion_data", "/f1_game/motion_data"), ("session_data", "/f1_game/session_data")]))
-    entries.append(launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_tf_updater', executable='tf_updater', output='screen', parameters=[LaunchConfiguration(config_file.name)], remappings=[("motion_data", "/f1_game/motion_data"), ("session_data", "/f1_game/session_data")]))
+    entries = [config_file,use_sim_time]
+    entries.append(launch_ros.actions.Node(package='deepracing_rclpy', name='f1_boundary_publisher', executable='boundary_publisher', output='screen', parameters=[{"track_search_dirs": os.getenv("F1_TRACK_DIRS","").split(os.pathsep), "use_sim_time" : LaunchConfiguration(use_sim_time.name)}], remappings=[("session_data", "/f1_game/session_data")]))
+    entries.append(launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_tf_updater', executable='tf_updater', output='screen', parameters=[LaunchConfiguration(config_file.name), {"use_sim_time" : LaunchConfiguration(use_sim_time.name)}], remappings=[("motion_data", "/f1_game/motion_data"), ("session_data", "/f1_game/session_data")]))
     return LaunchDescription(entries)
