2bf3320e8d8c6b503a2be4e3a681e691b352370f
Trent Weiss <ttw2xk@virginia.edu>
2021-11-02 15:38:16 -0400

    move some hard constants to ros params. Also move listeners to separate CB queue.

--- deepracing_rclcpp/src/autoware_control_node.cpp
+++ deepracing_rclcpp/src/autoware_control_node.cpp
@@ -1,7 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include <sstream>
 #include <functional>
-#include <deepracing_msgs/msg/timestamped_packet_motion_data.hpp>
+#include <deepracing_msgs/msg/timestamped_packet_car_telemetry_data.hpp>
+#include <deepracing_msgs/msg/timestamped_packet_car_status_data.hpp>
 #include <autoware_auto_msgs/msg/vehicle_control_command.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <control_toolbox/pid_ros.hpp>
@@ -17,39 +18,55 @@ class AutowareControlNode : public rclcpp::Node
     }
     void init(std::shared_ptr<control_toolbox::PidROS> pid)
     {
+      m_safe_vel_ = declare_parameter<double>("safe_vel", 24.0);
+      m_safe_steer_max_ = declare_parameter<double>("safe_steer_max", 0.135);
+      m_safe_steer_min_ = declare_parameter<double>("safe_steer_min", -0.1125);
+
+      m_full_lock_left_ = declare_parameter<double>("full_lock_left", 0.2986730635166168);
+      m_full_lock_right_ = declare_parameter<double>("full_lock_right", -0.26346784830093384);
+
       m_velocity_pid_ = pid;
-      m_velocity_pid_->initPid(1.25, 0.025, 0.005, 0.2, -0.2, true);
+      m_velocity_pid_->initPid(0.5, 0.05, 0.00, 1.0, -1.0, true);
       m_velocity_pid_->setCurrentCmd(0.0);
       m_velocity_pid_->computeCommand(0.0, rclcpp::Duration::from_seconds(0.0));
+
       m_game_interface_ = deepf1::F1InterfaceFactory::getDefaultInterface();
+      rclcpp::SubscriptionOptions listner_options;
+      listner_options.callback_group=create_callback_group(rclcpp::CallbackGroupType::Reentrant);
       command_listener = create_subscription<autoware_auto_msgs::msg::VehicleControlCommand>("ctrl_cmd", rclcpp::QoS{1},
-        std::bind(&AutowareControlNode::commandCallback, this, std::placeholders::_1));
+        std::bind(&AutowareControlNode::commandCallback, this, std::placeholders::_1), listner_options);
+      status_listener = create_subscription<deepracing_msgs::msg::TimestampedPacketCarStatusData>("/ego_vehicle/status_data", rclcpp::QoS{1},
+        std::bind(&AutowareControlNode::statusCallback, this, std::placeholders::_1), listner_options);
+      telemetry_listener = create_subscription<deepracing_msgs::msg::TimestampedPacketCarTelemetryData>("/ego_vehicle/telemetry_data", rclcpp::QoS{1},
+        std::bind(&AutowareControlNode::telemetryCallback, this, std::placeholders::_1), listner_options);
       odom_listener = create_subscription<nav_msgs::msg::Odometry>("/ego_vehicle/odom", rclcpp::QoS{1},
         std::bind(&AutowareControlNode::odomCallback, this, std::placeholders::_1));
+      
     }
     inline void controlLoop(const rclcpp::Duration& dt)
     {
-      double error = m_setpoints.velocity_mps-m_current_speed_;
-      double throttlecommand = m_velocity_pid_->computeCommand(m_setpoints.velocity_mps-m_current_speed_, dt);
-      // double throttlecommand;
-      // if (error>0.0)
-      // {
-      //   throttlecommand = 1.0;
-      // }
-      // else
-      // {
-      //   throttlecommand = -1.0;
-      // }
       double steercommand = m_setpoints.front_wheel_angle_rad;
-      deepf1::F1ControlCommand cmd;
-      if (steercommand>=0.0)
+      double error;
+      if (steercommand<=m_safe_steer_max_ && steercommand>=m_safe_steer_min_)
       {
-        cmd.steering=steercommand/0.2986730635166168;
+        error = m_setpoints.velocity_mps-m_current_speed_;
       }
       else
       {
-        cmd.steering=steercommand/0.26346784830093384;
+        error = m_safe_vel_-m_current_speed_;
       }
+
+      double throttlecommand = m_velocity_pid_->computeCommand(error, dt);
+      deepf1::F1ControlCommand cmd;
+      if (steercommand>=0.0)
+      {
+        cmd.steering=steercommand/m_full_lock_left_;
+      }
+      else
+      {
+        cmd.steering=-steercommand/m_full_lock_right_;
+      }
+      
       if(throttlecommand>=0.0)
       {
         cmd.throttle=throttlecommand;
@@ -61,25 +78,44 @@ class AutowareControlNode : public rclcpp::Node
         cmd.brake=-throttlecommand;
       }
       m_game_interface_->setCommands(cmd);
+      if (m_drs_allowed_ && (!m_drs_enabled_))
+      {
+        m_game_interface_->pushDRS();
+      }
     }
   private:
 
+    inline void telemetryCallback(const deepracing_msgs::msg::TimestampedPacketCarTelemetryData::SharedPtr telemetry_data)
+    {
+      m_drs_enabled_ = telemetry_data->udp_packet.car_telemetry_data.at(telemetry_data->udp_packet.header.player_car_index).drs>0;
+    }
+    inline void statusCallback(const deepracing_msgs::msg::TimestampedPacketCarStatusData::SharedPtr status_data)
+    {
+      m_drs_allowed_ = status_data->udp_packet.car_status_data.at(status_data->udp_packet.header.player_car_index).drs_allowed>0;
+    }
     inline void commandCallback(const autoware_auto_msgs::msg::VehicleControlCommand::SharedPtr new_commands)
     {
-      m_setpoints = *new_commands;
-      // m_setpoints.velocity_mps*=0.9;
+      autoware_auto_msgs::msg::VehicleControlCommand newsetpoints(*new_commands);
+      // newsetpoints.front_wheel_angle_rad*=2.0;
+      // newsetpoints.velocity_mps*=0.95;
+      m_setpoints = newsetpoints;
     }
     
     inline void odomCallback(const nav_msgs::msg::Odometry::SharedPtr odom)
     {
       m_current_speed_ = odom->twist.twist.linear.x;
     }
-    double m_current_speed_;
+    double m_current_speed_, m_safe_steer_max_, m_safe_steer_min_, m_safe_vel_, m_full_lock_left_, m_full_lock_right_;
+    bool m_drs_allowed_, m_drs_enabled_;
     std::shared_ptr<control_toolbox::PidROS> m_velocity_pid_;
     std::shared_ptr<deepf1::F1Interface> m_game_interface_;
     autoware_auto_msgs::msg::VehicleControlCommand m_setpoints;
     rclcpp::Subscription<autoware_auto_msgs::msg::VehicleControlCommand>::SharedPtr command_listener;
     rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_listener;
+    rclcpp::Subscription<deepracing_msgs::msg::TimestampedPacketCarStatusData>::SharedPtr status_listener;
+    rclcpp::Subscription<deepracing_msgs::msg::TimestampedPacketCarTelemetryData>::SharedPtr telemetry_listener;
+    
+    
 
 };
 int main(int argc, char *argv[]) {
@@ -87,6 +123,7 @@ int main(int argc, char *argv[]) {
   std::shared_ptr<AutowareControlNode> node(new AutowareControlNode(rclcpp::NodeOptions()));
   std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> executor( new rclcpp::executors::MultiThreadedExecutor(rclcpp::ExecutorOptions(), 3) );
   executor->add_node(node);
+  // rclcpp::Clock::SharedPtr clock = node->get_clock();
   rclcpp::Rate rate(node->declare_parameter<double>("frequency", 25.0));
   std::shared_ptr<control_toolbox::PidROS> pid_controller(new control_toolbox::PidROS(node));
   node->init(pid_controller);
