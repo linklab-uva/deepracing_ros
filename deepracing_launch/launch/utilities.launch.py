f7976c700b2af0b694cfd265d37ba1bc03a39582
Trent Weiss <ttw2xk@virginia.edu>
2021-11-04 12:15:42 -0400

    add another launch argument.

--- deepracing_launch/launch/utilities.launch.py
+++ deepracing_launch/launch/utilities.launch.py
@@ -11,9 +11,10 @@ def generate_launch_description():
     config_dir = os.path.join(get_package_share_directory("deepracing_launch"),"config")
     config_file = DeclareLaunchArgument("config_file", default_value=os.path.join(config_dir, "tf_updater.yaml"))
     use_sim_time = DeclareLaunchArgument("use_sim_time", default_value="false")
+    tf_from_odom = DeclareLaunchArgument("tf_from_odom", default_value="false")
     
     entries = [config_file,use_sim_time]
     entries.append(launch_ros.actions.Node(package='deepracing_rclpy', name='f1_boundary_publisher', executable='boundary_publisher', output='screen', parameters=[{"track_search_dirs": os.getenv("F1_TRACK_DIRS","").split(os.pathsep), "use_sim_time" : LaunchConfiguration(use_sim_time.name)}], remappings=[("session_data", "/f1_game/session_data")]))
     entries.append(launch_ros.actions.Node(package='deepracing_rclpy', name='vehicle_state_publisher', executable='vehicle_state_publisher', output='screen', parameters=[{"use_sim_time" : LaunchConfiguration(use_sim_time.name)}], remappings=[("motion_data", "/f1_game/motion_data"), ("lap_data", "/f1_game/lap_data")]))
-    entries.append(launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_tf_updater', executable='tf_updater', output='screen', parameters=[LaunchConfiguration(config_file.name), {"use_sim_time" : LaunchConfiguration(use_sim_time.name)}], remappings=[("motion_data", "/f1_game/motion_data"), ("session_data", "/f1_game/session_data")]))
+    entries.append(launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_tf_updater', executable='tf_updater', output='screen', parameters=[LaunchConfiguration(config_file.name), {"use_sim_time" : LaunchConfiguration(use_sim_time.name), "tf_from_odom" : LaunchConfiguration(tf_from_odom.name)}], remappings=[("motion_data", "/f1_game/motion_data"), ("session_data", "/f1_game/session_data")]))
     return LaunchDescription(entries)
