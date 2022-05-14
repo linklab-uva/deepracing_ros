3117faacd26071e1c49f854976dfee5f11ebb441
Trent Weiss <ttw2xk@virginia.edu>
2022-05-14 18:55:00 -0400

    declare a carname argument.

--- deepracing_launch/launch/utilities.launch.py
+++ deepracing_launch/launch/utilities.launch.py
@@ -14,12 +14,12 @@ def generate_launch_description():
     use_sim_time = DeclareLaunchArgument("use_sim_time", default_value="false")
     tf_from_odom = DeclareLaunchArgument("tf_from_odom", default_value="false")
     boundary_pub = DeclareLaunchArgument("boundary_pub", default_value="true")
-    ns = DeclareLaunchArgument("ns", default_value="")
+    carname = DeclareLaunchArgument("carname", default_value="")
     
-    entries = [boundary_pub,ns,config_file,use_sim_time,tf_from_odom]
+    entries = [boundary_pub, carname, config_file, use_sim_time, tf_from_odom]
     entries.append(launch_ros.actions.Node(package='deepracing_rclpy', name='f1_boundary_publisher', executable='boundary_publisher', output='screen',\
          parameters=[{"track_search_dirs": os.getenv("F1_TRACK_DIRS","").split(os.pathsep), use_sim_time.name : LaunchConfiguration(use_sim_time.name)}],\
-           condition=IfCondition(LaunchConfiguration(boundary_pub.name)), namespace=LaunchConfiguration(ns.name)))
-    entries.append(launch_ros.actions.Node(package='deepracing_rclpy', name='vehicle_state_publisher', executable='vehicle_state_publisher', output='screen', parameters=[{use_sim_time.name : LaunchConfiguration(use_sim_time.name)}], namespace=LaunchConfiguration(ns.name)))
-    entries.append(launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_tf_updater', executable='tf_updater', output='screen', parameters=[LaunchConfiguration(config_file.name), {use_sim_time.name : LaunchConfiguration(use_sim_time.name), tf_from_odom.name : LaunchConfiguration(tf_from_odom.name)}], namespace=LaunchConfiguration(ns.name)))
+           condition=IfCondition(LaunchConfiguration(boundary_pub.name)), namespace=LaunchConfiguration(carname.name)))
+    entries.append(launch_ros.actions.Node(package='deepracing_rclpy', name='vehicle_state_publisher', executable='vehicle_state_publisher', output='screen', parameters=[{use_sim_time.name : LaunchConfiguration(use_sim_time.name)}], namespace=LaunchConfiguration(carname.name)))
+    entries.append(launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_tf_updater', executable='tf_updater', output='screen', parameters=[LaunchConfiguration(config_file.name), {carname.name: LaunchConfiguration(carname.name), use_sim_time.name : LaunchConfiguration(use_sim_time.name), tf_from_odom.name : LaunchConfiguration(tf_from_odom.name)}], namespace=LaunchConfiguration(carname.name)))
     return LaunchDescription(entries)
