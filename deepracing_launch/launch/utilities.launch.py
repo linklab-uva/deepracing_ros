fbbfd160025b070fa7080cacb9625a556ac0b5df
Trent Weiss <ttw2xk@virginia.edu>
2022-05-15 07:28:30 -0400

    specify specific odom frame

--- deepracing_launch/launch/utilities.launch.py
+++ deepracing_launch/launch/utilities.launch.py
@@ -3,13 +3,16 @@
 import os
 import launch_ros.actions
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import FrontendLaunchDescriptionSource
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, Command
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    config_dir = os.path.join(get_package_share_directory("deepracing_launch"),"config")
+    deepracing_launch_dir = get_package_share_directory("deepracing_launch")
+    launch_dir = os.path.join(deepracing_launch_dir,"launch")
+    config_dir = os.path.join(deepracing_launch_dir,"config")
     config_file = DeclareLaunchArgument("config_file", default_value=os.path.join(config_dir, "tf_updater.yaml"))
     use_sim_time = DeclareLaunchArgument("use_sim_time", default_value="false")
     tf_from_odom = DeclareLaunchArgument("tf_from_odom", default_value="false")
@@ -22,4 +25,5 @@ def generate_launch_description():
            condition=IfCondition(LaunchConfiguration(boundary_pub.name)), namespace=LaunchConfiguration(carname.name)))
     entries.append(launch_ros.actions.Node(package='deepracing_rclpy', name='vehicle_state_publisher', executable='vehicle_state_publisher', output='screen', parameters=[{use_sim_time.name : LaunchConfiguration(use_sim_time.name)}], namespace=LaunchConfiguration(carname.name)))
     entries.append(launch_ros.actions.Node(package='deepracing_rclcpp', name='f1_tf_updater', executable='tf_updater', output='screen', parameters=[LaunchConfiguration(config_file.name), {carname.name: LaunchConfiguration(carname.name), use_sim_time.name : LaunchConfiguration(use_sim_time.name), tf_from_odom.name : LaunchConfiguration(tf_from_odom.name)}], namespace=LaunchConfiguration(carname.name)))
+    entries.append(IncludeLaunchDescription(FrontendLaunchDescriptionSource(os.path.join(launch_dir,"ekf.launch")), launch_arguments=[(carname.name, LaunchConfiguration(carname.name)), (use_sim_time.name, LaunchConfiguration(use_sim_time.name))], condition=IfCondition(LaunchConfiguration(tf_from_odom.name))))
     return LaunchDescription(entries)
