7488ff9b81740f31db5e1869ddf005fceba956f3
Trent Weiss <ttw2xk@virginia.edu>
2022-06-14 12:22:02 -0400

    add node for defaulting to Australia baseline for optimal line.

---
+++ deepracing_rclpy/deepracing_ros/scripts/initial_raceline_setter.py
@@ -0,0 +1,45 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+import rclpy.timer
+import rclpy.client
+import rclpy.executors
+from rclpy.node import Node
+from rclpy.publisher import Publisher
+from std_msgs.msg import String, Float64
+from nav_msgs.msg import Path
+import deepracing_msgs.srv
+from deepracing_msgs.msg import TimestampedPacketMotionData, CarMotionData, CarControl, BezierCurve
+from geometry_msgs.msg import PoseStamped, Pose
+from geometry_msgs.msg import PointStamped, Point
+import numpy as np
+from scipy.spatial.transform import Rotation as Rot
+from deepracing_ros.controls.path_server_oracle_raceline import OraclePathServer
+from deepracing_ros.utils import AsyncSpinner
+from rclpy.executors import MultiThreadedExecutor
+class RacelineSetter(Node):
+    def __init__(self, name="initial_raceline_setter"):
+        super(RacelineSetter, self).__init__(name)
+
+def main(args=None):
+    rclpy.init(args=args)
+    rclpy.logging.initialize()
+    node = RacelineSetter()
+    serviceclient : rclpy.client.Client = node.create_client(deepracing_msgs.srv.SetRaceline, "set_raceline")
+    serviceclient.wait_for_service()
+    req : deepracing_msgs.srv.SetRaceline.Request = deepracing_msgs.srv.SetRaceline.Request()
+    req.filename="Australia_baseline.json"
+    serviceclient.call_async(req)
+    rclpy.spin_once(node)
+if __name__ == '__main__':
+    main()
