f2193b44fed8a2603e4daa1a0f5abd8b9e7a6704
Trent Weiss <ttw2xk@virginia.edu>
2022-06-20 13:17:37 -0400

    finish up boilerplate code

--- deepracing_rclcpp/src/race_supervisor.cpp
+++ deepracing_rclcpp/src/race_supervisor.cpp
@@ -4,6 +4,10 @@
 #include <ackermann_msgs/msg/ackermann_drive_stamped.hpp>
 #include <std_msgs/msg/float64.hpp>
 #include <nav_msgs/msg/odometry.hpp>
+#include <deepracing_msgs/msg/race_supervisor_state.hpp>
+#include <chrono>
+
+using namespace std::chrono_literals;
 
 class RaceSupervisorNode : public rclcpp::Node 
 {
@@ -12,15 +16,32 @@ class RaceSupervisorNode : public rclcpp::Node
     RaceSupervisorNode( const rclcpp::NodeOptions & options )
      : rclcpp::Node("race_supervisor", options)
     {
+      m_frequency_ = declare_parameter<double>("frequency", 10.0);
+      m_timer_ = create_wall_timer(std::chrono::microseconds((int)std::round(1.0E6/m_frequency_)), std::bind(&RaceSupervisorNode::main_loop, this));
     }
+  private:
+    void main_loop()
+    {
+      RCLCPP_INFO(this->get_logger(), "Hello, world!");
+    }
+    double m_frequency_;
+    rclcpp::TimerBase::SharedPtr m_timer_;
 };
 int main(int argc, char *argv[]) {
   rclcpp::init(argc,argv);
   rclcpp::NodeOptions opshinz;
   opshinz = opshinz.allow_undeclared_parameters(true);
   std::shared_ptr<RaceSupervisorNode> node(new RaceSupervisorNode(opshinz));
-  rclcpp::Rate rate(node->get_parameter("frequency").as_double());
-  std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> executor( new rclcpp::executors::MultiThreadedExecutor(rclcpp::ExecutorOptions(), 3) );
+  int num_threads_ = node->declare_parameter<int>("num_threads", 0), num_threads;
+  if (num_threads_<=0)
+  {
+    num_threads = 0;
+  }
+  else
+  {
+    num_threads = num_threads_;
+  }
+  std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> executor( new rclcpp::executors::MultiThreadedExecutor(rclcpp::ExecutorOptions(), num_threads) );
   executor->add_node(node);
   executor->spin();
 }
