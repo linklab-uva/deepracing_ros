8b16fb78523ca6f5fc6c300cb6eedbbe64e6ef25
Trent Weiss <ttw2xk@virginia.edu>
2023-02-10 14:51:05 -0500

    make utilities library separate visibility control.

--- deepracing_rclcpp/include/deepracing_ros/utils/file_utils.h
+++ deepracing_rclcpp/include/deepracing_ros/utils/file_utils.h
@@ -1,6 +1,6 @@
 #ifndef DEEPRACING_ROS_FILE_UTILS_H
 #define DEEPRACING_ROS_FILE_UTILS_H
-#include <deepracing_ros/visibility_control.hpp>
+#include <deepracing_ros/utils/visibility_control.hpp>
 #include <vector>
 #include <string>
 
@@ -10,7 +10,7 @@ namespace deepracing_ros
     class FileUtils
     {
         public:
-            static inline DEEPRACING_RCLCPP_PUBLIC char pathsep()
+            static inline DEEPRACING_RCLCPP_UTILS_PUBLIC char pathsep()
             {
                 #ifdef _MSC_VER
                     return ';';
@@ -18,8 +18,8 @@ namespace deepracing_ros
                     return ':';
                 #endif
             }
-            static DEEPRACING_RCLCPP_PUBLIC std::string findFile(const std::string& file, const std::vector<std::string> & search_dirs);
-            static DEEPRACING_RCLCPP_PUBLIC std::vector<std::string> split(const std::string& str, char delim=pathsep());
+            static DEEPRACING_RCLCPP_UTILS_PUBLIC std::string findFile(const std::string& file, const std::vector<std::string> & search_dirs);
+            static DEEPRACING_RCLCPP_UTILS_PUBLIC std::vector<std::string> split(const std::string& str, char delim=pathsep());
     };
 }
 
