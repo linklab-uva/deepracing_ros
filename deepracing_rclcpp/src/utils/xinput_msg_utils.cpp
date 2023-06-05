dc058d65f627e987e29ed25b36848bf45cea527b
Trent Weiss <ttw2xk@virginia.edu>
2023-06-05 11:39:03 -0400

    inplace method for update controller

--- deepracing_rclcpp/src/utils/xinput_msg_utils.cpp
+++ deepracing_rclcpp/src/utils/xinput_msg_utils.cpp
@@ -40,5 +40,31 @@ namespace deepracing_ros
         rtn.thumb_ry = xinput_gamepad.sThumbRY;
         return rtn;
     }
+    XINPUT_GAMEPAD XinputMsgUtils::toXinput(const deepf1::F1ControlCommand& f1_interface_command)
+    {
+        XINPUT_GAMEPAD rtn;
+        toXinputInplace(f1_interface_command, rtn);
+        return rtn;
+    }
+    void XinputMsgUtils::toXinputInplace(const deepf1::F1ControlCommand& f1_interface_command, XINPUT_GAMEPAD& rtn)
+    {
+        //short goes from -32,768 to 32,767
+        //but left (negative) on the joystick is steering to the left, which is a positive steering angle in our convention.
+        double steering = std::clamp<double>(f1_interface_command.steering, -1.0, 1.0);
+        if (steering>0.0)
+        {
+            rtn.sThumbLX = (SHORT)std::round(-32768.0*steering);
+        }
+        else
+        {
+            rtn.sThumbLX = (SHORT)std::round(-32767.0*steering);
+        }
+        rtn.sThumbLY=rtn.sThumbRX=rtn.sThumbRY=0;
+        //BYTE (unsigned char) goes from 0 to 255
+        double throttle = std::clamp<double>(f1_interface_command.throttle, -1.0, 1.0);
+        double brake = std::clamp<double>(f1_interface_command.brake, -1.0, 1.0);
+        rtn.bRightTrigger = (BYTE)std::round(255.0*throttle);
+        rtn.bLeftTrigger = (BYTE)std::round(255.0*brake);
+    }
 
 }
