e408c2daea8f7d7211654f983097f26a67372ee7
Trent Weiss <ttw2xk@virginia.edu>
2023-06-05 18:03:32 -0400

    fix an oopsie

--- deepracing_rclcpp/src/composable_nodes/multiagent_control_node.cpp
+++ deepracing_rclcpp/src/composable_nodes/multiagent_control_node.cpp
@@ -4,6 +4,7 @@
 #include <f1_datalogger/controllers/multiagent_f1_interface_factory.h>
 #include <deepracing_msgs/msg/xinput_state.hpp>
 #include <deepracing_ros/utils/xinput_msg_utils.h>
+#include <std_msgs/msg/float64.hpp>
 #include <memory>
 #include <unordered_map>
 
@@ -33,10 +34,22 @@ class MultiagentControlNode : public rclcpp::Node
         {
           interfaces_[driver] = std::static_pointer_cast<deepf1::VigemInterface>(factory_.createInterface());
           std::string direct_topic_name = "/" + driver + "/controller_override";
-          std::function<void(const deepracing_msgs::msg::XinputState::ConstPtr&)> func
+
+          std::function<void(const deepracing_msgs::msg::XinputState::ConstPtr&)> func_override
           = std::bind(&MultiagentControlNode::setStateDirect_, this, std::placeholders::_1,  driver);
-          direct_subscriptions_[driver] = create_subscription<deepracing_msgs::msg::XinputState>(direct_topic_name, rclcpp::SensorDataQoS(), func);
-          last_direct_input_[driver] = rclcpp::Time();
+          direct_subscriptions_[driver] = create_subscription<deepracing_msgs::msg::XinputState>(direct_topic_name, rclcpp::SensorDataQoS(), func_override);
+
+          std::string steer_topic_name = "/" + driver + "/steering";
+          std::function<void(const std_msgs::msg::Float64::ConstPtr&)> func_steer
+          = std::bind(&MultiagentControlNode::setSteering_, this, std::placeholders::_1,  driver);
+          steer_subscriptions_[driver] = create_subscription<std_msgs::msg::Float64>(steer_topic_name, rclcpp::SensorDataQoS(), func_steer);
+
+          std::string accel_topic_name = "/" + driver + "/acceleration";
+          std::function<void(const std_msgs::msg::Float64::ConstPtr&)> func_accel
+          = std::bind(&MultiagentControlNode::setAcceleration_, this, std::placeholders::_1,  driver);
+          accel_subscriptions_[driver] = create_subscription<std_msgs::msg::Float64>(accel_topic_name, rclcpp::SensorDataQoS(), func_accel);
+
+          last_direct_input_[driver] = deepracing_msgs::msg::XinputState();
         }
       }
       catch(const std::exception& e)
@@ -48,14 +61,51 @@ class MultiagentControlNode : public rclcpp::Node
     deepf1::MultiagentF1InterfaceFactory factory_;
     std::unordered_map<std::string, std::shared_ptr<deepf1::VigemInterface>> interfaces_;
     std::unordered_map<std::string, rclcpp::Subscription<deepracing_msgs::msg::XinputState>::SharedPtr> direct_subscriptions_;
-    std::unordered_map<std::string, rclcpp::Time> last_direct_input_;
+    std::unordered_map<std::string, rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr> steer_subscriptions_;
+    std::unordered_map<std::string, rclcpp::Subscription<std_msgs::msg::Float64>::SharedPtr> accel_subscriptions_;
+    std::unordered_map<std::string, deepracing_msgs::msg::XinputState> last_direct_input_;
     rclcpp::TimerBase::SharedPtr timer_;
 
     void setStateDirect_(const deepracing_msgs::msg::XinputState::ConstPtr& state, const std::string& driver)
     {
       RCLCPP_INFO(get_logger(), "Setting state directly for driver %s", driver.c_str());
       interfaces_[driver]->setStateDirectly(deepracing_ros::XinputMsgUtils::toXinput(*state));
-      last_direct_input_[driver] = state->header.stamp;
+      last_direct_input_[driver] = *state;
+    }
+    void setSteering_(const std_msgs::msg::Float64::ConstPtr& steering_angle, const std::string& driver)
+    {
+      rclcpp::Time now = get_clock()->now();
+      rclcpp::Duration dt = now - last_direct_input_[driver].header.stamp;
+      if (dt<rclcpp::Duration::from_seconds(0.25))
+      {
+        return;
+      }
+      deepf1::F1ControlCommand cmd;
+      cmd.brake = cmd.throttle = std::nan("");
+      cmd.steering = steering_angle->data;
+      interfaces_[driver]->setCommands(cmd);
+    }
+    void setAcceleration_(const std_msgs::msg::Float64::ConstPtr& accel, const std::string& driver)
+    {
+      rclcpp::Time now = get_clock()->now();
+      rclcpp::Duration dt = now - last_direct_input_[driver].header.stamp;
+      if (dt<rclcpp::Duration::from_seconds(0.25))
+      {
+        return;
+      }
+      deepf1::F1ControlCommand cmd;
+      cmd.steering = std::nan("");
+      if(accel->data>0.0)
+      {
+        cmd.throttle = accel->data;
+        cmd.brake = 0.0;
+      }
+      else
+      {
+        cmd.throttle = 0.0;
+        cmd.brake = accel->data;
+      }
+      interfaces_[driver]->setCommands(cmd);
     }
 };
 
