4b41c5e269c6559b4332bf83b6c8d4220724511c
Trent Weiss <ttw2xk@virginia.edu>
2023-06-05 15:35:11 -0400

    change argument names

--- deepracing_rclcpp/src/utils/xinput_msg_utils.cpp
+++ deepracing_rclcpp/src/utils/xinput_msg_utils.cpp
@@ -1,23 +1,23 @@
 #include <deepracing_ros/utils/xinput_msg_utils.h>
 namespace deepracing_ros
 {
-    XINPUT_GAMEPAD XinputMsgUtils::toXinput(const deepracing_msgs::msg::XinputGamepad& joy_msg)
+    XINPUT_GAMEPAD XinputMsgUtils::toXinput(const deepracing_msgs::msg::XinputGamepad& xinput_msg)
     {
         XINPUT_GAMEPAD rtn;
-        rtn.wButtons = joy_msg.buttons;
-        rtn.bLeftTrigger = joy_msg.left_trigger;
-        rtn.bRightTrigger = joy_msg.right_trigger;
-        rtn.sThumbLX = joy_msg.thumb_lx;
-        rtn.sThumbLY = joy_msg.thumb_ly;
-        rtn.sThumbRX = joy_msg.thumb_rx;
-        rtn.sThumbRY = joy_msg.thumb_ry;
+        rtn.wButtons = xinput_msg.buttons;
+        rtn.bLeftTrigger = xinput_msg.left_trigger;
+        rtn.bRightTrigger = xinput_msg.right_trigger;
+        rtn.sThumbLX = xinput_msg.thumb_lx;
+        rtn.sThumbLY = xinput_msg.thumb_ly;
+        rtn.sThumbRX = xinput_msg.thumb_rx;
+        rtn.sThumbRY = xinput_msg.thumb_ry;
         return rtn;
     }
-    XINPUT_STATE XinputMsgUtils::toXinput(const deepracing_msgs::msg::XinputState& joy_msg)
+    XINPUT_STATE XinputMsgUtils::toXinput(const deepracing_msgs::msg::XinputState& xinput_msg)
     {
         XINPUT_STATE rtn;
-        rtn.dwPacketNumber = joy_msg.packet_number;
-        rtn.Gamepad = toXinput(joy_msg.gamepad);
+        rtn.dwPacketNumber = xinput_msg.packet_number;
+        rtn.Gamepad = toXinput(xinput_msg.gamepad);
         return rtn;
     }
 
