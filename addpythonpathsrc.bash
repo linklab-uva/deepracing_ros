7cda97b227b3e9d71277e437bbe7ded179df4110
Trent Weiss <ttw2xk@virginia.edu>
2023-07-11 09:05:53 -0400

    update pythonpath script

--- addpythonpathsrc.bash
+++ addpythonpathsrc.bash
@@ -8,12 +8,18 @@ cd `dirname ${SCRIPT_PATH}` > /dev/null
 SCRIPT_PATH=`pwd`;
 popd  > /dev/null
 
-echo $SCRIPT_PATH
 extrapythonpaths=$SCRIPT_PATH/deepracing_rclpy
-echo $extrapythonpaths
+echo "Adding $extrapythonpaths to PYTHONPATH"
 
 if [[ -z "${PYTHONPATH}" ]]; then
   export PYTHONPATH=${extrapythonpaths}
 else
   export PYTHONPATH=${extrapythonpaths}:${PYTHONPATH}
+fi
+
+mapsdir=$SCRIPT_PATH/deepracing_launch/maps
+if [[ -z "${F1_MAP_DIRS}" ]]; then
+  export F1_MAP_DIRS=${mapsdir}
+else
+  export F1_MAP_DIRS=${mapsdir}:${F1_MAP_DIRS}
 fi
