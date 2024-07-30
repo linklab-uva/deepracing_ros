f0e64cfda0d63ac5221a8450e45ebd6ede333a27
Trent Weiss <ttw2xk@virginia.edu>
2024-07-30 10:33:51 -0400

    add TrackMapPublisher to the container. Also include use_sim_time as optional param

--- deepracing_launch/launch/ros_rebroadcaster.launch.py
+++ deepracing_launch/launch/ros_rebroadcaster.launch.py
@@ -21,58 +21,83 @@ def generate_launch_description():
     argz.append(port)
     allcars = launch.actions.DeclareLaunchArgument("publish_all_cars", default_value="false")
     argz.append(allcars)
+    use_sim_time = launch.actions.DeclareLaunchArgument("use_sim_time", default_value="false")
+    argz.append(use_sim_time)
+
+    search_dirs = []
+    search_dirs_env = os.getenv("F1_MAP_DIRS", None)
+    if search_dirs_env is not None:
+        search_dirs.extend(search_dirs_env.split(os.pathsep))
+    search_dirs.append(os.path.join(deepracing_launch_dir,"maps"))
     composable_nodez = [
         launch_ros.descriptions.ComposableNode(
             package='udp_driver',
             plugin='drivers::udp_driver::UdpReceiverNode',
             name='raw_udp_receiver_node',
             remappings=[('udp_read', '_all_udp_in')],
-            parameters=[{ip.name : launch.substitutions.LaunchConfiguration(ip.name), port.name : launch.substitutions.LaunchConfiguration(port.name)}],
+            parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name), 
+                         ip.name : launch.substitutions.LaunchConfiguration(ip.name), 
+                         port.name : launch.substitutions.LaunchConfiguration(port.name)}],
             extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]),
         launch_ros.descriptions.ComposableNode(
             package='deepracing_rclcpp',
             plugin='deepracing::composable_nodes::UdpDemuxer',
             name='udp_demuxer_node',
+            parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name)}],
             extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]),
         launch_ros.descriptions.ComposableNode(
             package='deepracing_rclcpp',
             plugin='deepracing::composable_nodes::ReceiveCarSetupData',
             name='car_setup_node',
-            parameters=[{'all_cars': launch.substitutions.LaunchConfiguration(allcars.name)}],
+            parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name), 
+                         'all_cars': launch.substitutions.LaunchConfiguration(allcars.name)}],
             extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]),
         launch_ros.descriptions.ComposableNode(
             package='deepracing_rclcpp',
             plugin='deepracing::composable_nodes::ReceiveCarStatusData',
             name='car_status_node',
-            parameters=[{'all_cars': launch.substitutions.LaunchConfiguration(allcars.name)}],
+            parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name), 
+                         'all_cars': launch.substitutions.LaunchConfiguration(allcars.name)}],
             extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]),
         launch_ros.descriptions.ComposableNode(
             package='deepracing_rclcpp',
             plugin='deepracing::composable_nodes::ReceiveLapData',
             name='lap_data_node',
-            parameters=[{'all_cars': launch.substitutions.LaunchConfiguration(allcars.name)}],
+            parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name), 
+                         'all_cars': launch.substitutions.LaunchConfiguration(allcars.name)}],
             extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]),
         launch_ros.descriptions.ComposableNode(
             package='deepracing_rclcpp',
             plugin='deepracing::composable_nodes::ReceiveMotionData',
             name='motion_data_node',
-            parameters=[{'all_cars': launch.substitutions.LaunchConfiguration(allcars.name)}],
+            parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name), 
+                         'all_cars': launch.substitutions.LaunchConfiguration(allcars.name)}],
             extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]),
         launch_ros.descriptions.ComposableNode(
             package='deepracing_rclcpp',
             plugin='deepracing::composable_nodes::ReceiveMotionExData',
             name='motion_data_ex_node',
+            parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name)}],
             extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]),
         launch_ros.descriptions.ComposableNode(
             package='deepracing_rclcpp',
             plugin='deepracing::composable_nodes::ReceiveSessionData',
             name='session_data_node',
+            parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name)}],
             extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]),
         launch_ros.descriptions.ComposableNode(
             package='deepracing_rclcpp',
             plugin='deepracing::composable_nodes::ReceiveTelemetryData',
             name='telemetry_data_node',
-            parameters=[{'all_cars': launch.substitutions.LaunchConfiguration(allcars.name)}],
+            parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name), 
+                         'all_cars': launch.substitutions.LaunchConfiguration(allcars.name)}],
+            extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}]),
+        launch_ros.descriptions.ComposableNode(
+            package='deepracing_rclcpp',
+            plugin='deepracing::composable_nodes::TrackmapPublisher',
+            name='trackmap_publisher_node',
+            parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name), 
+                         'search_dirs': search_dirs}],
             extra_arguments=[{'use_intra_process_comms': use_intra_process_comms}])
     ]
     container = launch_ros.actions.ComposableNodeContainer(
@@ -80,15 +105,18 @@ def generate_launch_description():
         namespace='udp_interface',
         package='rclcpp_components',
         executable='component_container_mt',
-        parameters=[{"thread_num" : len(composable_nodez)+1}],
+        parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name), 
+                     "thread_num" : len(composable_nodez)+1}],
         composable_node_descriptions=composable_nodez,
         output='both'
     )
     nodez = []
     nodez.append(launch_ros.actions.Node(package='deepracing_rclpy', 
                                          name='initialize_udp_receiver', 
-                                         executable='initialize_udp_receiver', 
+                                         executable='initialize_lifecycle_node', 
                                          namespace="udp_interface",
+                                         parameters=[{use_sim_time.name : launch.substitutions.LaunchConfiguration(use_sim_time.name), 
+                                                      "node_to_initialize" : "raw_udp_receiver_node"}],
                                          output='screen'))
     
     return launch.LaunchDescription(argz + nodez + [container,])
