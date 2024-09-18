2f6286a49d54278e18b2b28dd877dbbc47f2ca9f
Trent Weiss <ttw2xk@virginia.edu>
2024-09-18 15:03:37 -0400

    sure

--- deepracing_rclcpp/src/composable_nodes/trackmap_publisher.cpp
+++ deepracing_rclcpp/src/composable_nodes/trackmap_publisher.cpp
@@ -26,9 +26,11 @@ namespace composable_nodes
             DEEPRACING_RCLCPP_PUBLIC TrackmapPublisher(const rclcpp::NodeOptions & options) : 
                 rclcpp::Node("trackmap_publisher", options), names_map_(deepracing::Utils::trackNames())
             {
+
                 map_to_track_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
                 rclcpp::QoS qos = rclcpp::SystemDefaultsQoS().keep_last(10).durability_volatile();
                 search_dirs_ = declare_parameter<std::vector<std::string>>("search_dirs");
+                transform_to_map_= declare_parameter<bool>("transform_to_map", true);
                 
                 get_line_srv_ = create_service<deepracing_msgs::srv::GetLine>("get_line", 
                     std::bind(&TrackmapPublisher::getline_cb, this, std::placeholders::_1, std::placeholders::_2));
@@ -41,6 +43,7 @@ namespace composable_nodes
                     std::bind(&TrackmapPublisher::timer_cb, this));                
             }   
         private:
+            bool transform_to_map_;
             std::map<std::int8_t, std::string> names_map_;
             std::shared_ptr<tf2_ros::StaticTransformBroadcaster> map_to_track_broadcaster_;
             rclcpp::TimerBase::SharedPtr timer_;
@@ -50,26 +53,27 @@ namespace composable_nodes
             rclcpp::Subscription<deepracing_msgs::msg::TimestampedPacketSessionData>::SharedPtr session_sub_;
             rclcpp::Service<deepracing_msgs::srv::GetLine>::SharedPtr get_line_srv_;
             std::mutex mutex_;
-            void getline_cb(const deepracing_msgs::srv::GetLine::Request::SharedPtr request,
+            bool getline_cb(const deepracing_msgs::srv::GetLine::Request::SharedPtr request,
                 deepracing_msgs::srv::GetLine::Response::SharedPtr response)
             {
                 std::scoped_lock lock(mutex_);
                 if(!track_map_)
                 {
                     response->return_code=deepracing_msgs::srv::GetLine::Response::TRACKMAP_NOT_INITIALIZED;
-                    return;
+                    return false;
                 }
                 try{
                     const pcl::PCLPointCloud2& pc2 = track_map_->getCloud(request->key.data);
                     pcl_conversions::fromPCL(pc2, response->line);
                 }catch(std::out_of_range& e){
                     response->return_code=deepracing_msgs::srv::GetLine::Response::KEY_NOT_FOUND;
-                    return;
+                    return  false;
                 }catch(std::exception& e){
                     response->return_code=deepracing_msgs::srv::GetLine::Response::UNKNOWN;
-                    return;
+                    return false;
                 }
                 response->return_code=deepracing_msgs::srv::GetLine::Response::SUCCESS;
+                return true;
             }
             void session_cb(deepracing_msgs::msg::TimestampedPacketSessionData::UniquePtr session_data)
             {
@@ -82,7 +86,7 @@ namespace composable_nodes
                 if(!track_map_ || track_map_->name()!=tracknamein)
                 {
                     track_map_.reset();
-                    track_map_ = deepracing::TrackMap::findTrackmap(tracknamein, search_dirs_);
+                    track_map_ = deepracing::TrackMap::findTrackmap(tracknamein, search_dirs_, transform_to_map_);
                     if(track_map_){
                         RCLCPP_INFO(get_logger(), "Got map for track name %s", track_map_->name().c_str());
                         if(track_map_->widthMap() && !(widthmap_pub_))
@@ -108,17 +112,16 @@ namespace composable_nodes
                 }
                 const rclcpp::Time& now = get_clock()->now();
                 geometry_msgs::msg::TransformStamped map_to_track_msg = tf2::eigenToTransform(track_map_->startinglinePose().inverse());
-                map_to_track_msg.header.set__stamp(now);
-                map_to_track_msg.header.frame_id="map";
-                map_to_track_msg.child_frame_id="track";
+                map_to_track_msg.header.set__stamp(now).set__frame_id("map");
+                map_to_track_msg.set__child_frame_id("track");
                 map_to_track_broadcaster_->sendTransform(map_to_track_msg);
                 sensor_msgs::msg::PointCloud2 innerbound_msg, outerbound_msg, racline_msg, asdf;
                 pcl::toROSMsg<deepracing::PointXYZLapdistance>(*track_map_->innerBound(), innerbound_msg);
                 pcl::toROSMsg<deepracing::PointXYZLapdistance>(*track_map_->outerBound(), outerbound_msg);
                 pcl::toROSMsg<deepracing::PointXYZTime>(*track_map_->raceline(), racline_msg);
                 innerbound_msg.header.set__stamp(now);
-                racline_msg.header.set__stamp(now);
                 outerbound_msg.header.set__stamp(now);
+                racline_msg.header.set__stamp(now);
                 ib_pub_->publish(innerbound_msg);
                 ob_pub_->publish(outerbound_msg);
                 raceline_pub_->publish(racline_msg);
